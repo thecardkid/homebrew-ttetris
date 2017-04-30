92a02ade663cfe1ca8f1c0be04205ce8487173dd
Hieu Nguyen <hieu.nguyen@students.olin.edu>
2017-04-30 13:57:12 -0400

    Upgrade to version 1.1.0

--- terminally-tetris.rb
+++ terminally-tetris.rb
@@ -1,8 +1,8 @@
 class TerminallyTetris < Formula
   desc "Play Tetris in the Terminal"
   homepage "https://github.com/thecardkid/terminally-tetris"
-  url "https://github.com/thecardkid/terminally-tetris/archive/v1.0.0.tar.gz"
-  sha256 "213f8a7581e69d3284291187eb887a1c0822fe699d53a7768f0fea777bc1427b"
+  url "https://github.com/thecardkid/terminally-tetris/archive/v1.1.0.tar.gz"
+  sha256 "f1777b45233c0276dd86478cb49c27e28066a1b5750284a84554f71ed1c2e6da"
 
   depends_on "ncurses" => :build
 
